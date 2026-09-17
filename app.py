a1cab4df2079974e37a3b5fe256aa422fce93a49
vaishnavik2024a-hash <vaishnavi.k2024a@vitstudent.ac.in>
2026-09-17 15:03:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+with open("exam_result.txt", "w") as f:
+ f.write("Online Examination Result\n")
+ f.write("Student Name: Priya\n")
+ f.write("Register Number: 24BCE0001\n")
+ f.write("Total Questions: 10\n")
+ f.write("Correct Answers: 8\n")
+ f.write("Score: 8/10\n")
+ f.write("Result: PASS\n")
+print("Exam result generated.")
